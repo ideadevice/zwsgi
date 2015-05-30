94b97aa69e1e3a134e4d39a1b0236102f84f19bc
Balaji J <b@ideadevice.com>
2015-05-30 12:35:59 +0530

    Add author

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name='zwsgi',
     version='0.0.1',
     url='https://github.com/ideadevice/zwsgi',
-    author='ID Engg.',
+    author='Balaji J (Idea Device)',
     install_requires=install_reqs,
     description='zeromq wsgi bridge',
     packages=find_packages(),
