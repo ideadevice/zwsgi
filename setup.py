d751ddb537efe16dd52e740e7d06419189508a4e
Balaji J <b@ideadevice.com>
2015-05-30 12:32:17 +0530

    make version as 0.0.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ install_reqs = [
 
 setup(
     name='calm',
-    version='0.1.0',
+    version='0.0.1',
     url='https://github.com/ideadevice/zwsgi',
     author='ID Engg.',
     install_requires=install_reqs,
